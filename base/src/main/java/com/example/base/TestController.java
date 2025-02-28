b015834c1fbefdc296ec987dee9d2c93241710e6
moralopez-a <amoralopez@csumb.edu>
2025-02-27 21:48:19 -0800

    working spring boot & docker

---
+++ base/src/main/java/com/example/base/TestController.java
@@ -0,0 +1,25 @@
+package com.example.base;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test") // Base path for the route
+public class TestController {
+
+    @GetMapping
+    public String testEndpoint() {
+        return "Test endpoint is working!";
+    }
+}
+
+
+
+
+
+
+
+
+
+
